b0281d39f1621238ad8e981fb1a648c6d7daa0ea
khuuvannguyen <khuuvannguyen@gmail.com>
2022-09-26 16:09:57 +0700

    initial

---
+++ src/main/java/com/nguyen/app/App.java
@@ -0,0 +1,13 @@
+package com.nguyen.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
